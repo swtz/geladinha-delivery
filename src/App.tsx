f3eeb3c51ec5632ae58939415f6d7a5fe44cf487
Leonardo Lüders <luderser@hotmail.com>
2025-05-28 14:22:14 -0300

    Installed Tailwind CSS and clsx packages and created the Container styled component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import clsx from "clsx";
+import "./styles/global.css";
+import { Container } from "./components/Container";
+
 export default function App() {
   return (
-    <h1>Hello, world!</h1>
-  )
+    <Container>
+      <h1 className={clsx("text-center bg-amber-200")}>Hello, world!</h1>
+    </Container>
+  );
 }
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import clsx from "clsx";
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+  return <div className={clsx("bg-amber-600 max-w-5xl mx-auto")}>
+    <div className="m-8">{children}</div>
+  </div>;
+}
