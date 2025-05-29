6c32b6359cc3643f577ab835d4519e4eb6078c93
Leonardo Lüders <luderser@hotmail.com>
2025-05-29 10:04:10 -0300

    Styled the card and separated its content into divs.

--- src/components/DeliveryCard/index.tsx
+++ src/components/DeliveryCard/index.tsx
@@ -1,13 +1,28 @@
+import clsx from "clsx";
 import { CurrencyValue } from "../CurrencyValue";
 
 export function DeliveryCard() {
   return (
-    <div className="p-4 bg-orange-300">
-      <h2 className="text-2xl text-lime-950">Cliente: Lion Lurdes</h2>
-      <span className="italic text-slate-900">Criado em 27/05/25 às 19:00</span>
-      <p className="text-lg">
-        Tipo de entrega: <b>em casa</b>
-      </p>
+    <div
+      className={clsx(
+        "max-w-sm p-4",
+        "bg-orange-300 text-black",
+        "rounded-lg",
+        "border-2 border-orange-400",
+        "cursor-pointer transition",
+        "hover:brightness-110"
+      )}
+    >
+      <div>
+        <h3 className="text-2xl text-lime-950">Cliente:</h3>
+        <h2 className="text-xl">Lion Lurdes</h2>
+        <span className="italic text-slate-900">
+          Criado em 27/05/25 às 19:00
+        </span>
+        <p className="text-lg">
+          Tipo de entrega: <b>em casa</b>
+        </p>
+      </div>
       <div>
         <h3 className="text-2xl text-lime-950">Endereço de entrega:</h3>
         <ul className="list-disc pl-8">
@@ -17,18 +32,21 @@ export function DeliveryCard() {
           <li>Centro, Balneário Gaivota - SC</li>
         </ul>
       </div>
-      <p>
-        Método de pagamento: <b>Cartão</b>
-      </p>
-      <p>
-        Subtotal: <CurrencyValue value={89.0} />
-      </p>
-      <p>
-        Taxa de entrega: <CurrencyValue value={2.0} />
-      </p>
-      <p>
-        Total: <CurrencyValue value={91.0} />
-      </p>
+      <div>
+        <h3 className="text-2xl text-lime-950">Pagamento:</h3>
+        <p>
+          Método de pagamento: <b>Cartão</b>
+        </p>
+        <p>
+          Subtotal: <CurrencyValue value={89.0} />
+        </p>
+        <p>
+          Taxa de entrega: <CurrencyValue value={2.0} />
+        </p>
+        <p>
+          Total: <CurrencyValue value={91.0} />
+        </p>
+      </div>
     </div>
   );
 }
