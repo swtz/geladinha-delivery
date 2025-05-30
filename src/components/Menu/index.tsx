0db03661c0290483bb901f32302759705af642c3
Leonardo Lüders <luderser@hotmail.com>
2025-05-30 16:20:22 -0300

    Removed JSX conditional to fix hover on ItemMenu.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -29,29 +29,27 @@ export function Menu({ toggleDropdown, isActive }: MenuProps) {
           onClick={toggleDropdown}
         >
           Pedidos
-          {isActive && (
-            <ul
-              className={clsx(
-                "hidden",
-                "absolute z-20",
-                "top-[30px] p-4",
-                "bg-orange-400 border-2 border-orange-500",
-                "before:content-[''] before:absolute",
-                "before:top-[-8px] before:left-0 before:w-0 before:h-0",
-                "before:border-b-[8px]",
-                "before:border-l-transparent before:border-l-[10px]",
-                "before:border-r-transparent before:border-r-[10px]"
-              )}
-              onClick={(e) => e.stopPropagation()}
-            >
-              <li>
-                <a href="#">Motoboy</a>
-              </li>
-              <li>
-                <a href="#">Balcão</a>
-              </li>
-            </ul>
-          )}
+          <ul
+            className={clsx(
+              "hidden",
+              "absolute z-20",
+              "top-[30px] p-4",
+              "bg-orange-400 border-2 border-orange-500",
+              "before:content-[''] before:absolute",
+              "before:top-[-8px] before:left-0 before:w-0 before:h-0",
+              "before:border-b-[8px]",
+              "before:border-l-transparent before:border-l-[10px]",
+              "before:border-r-transparent before:border-r-[10px]"
+            )}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <li>
+              <a href="#">Motoboy</a>
+            </li>
+            <li>
+              <a href="#">Balcão</a>
+            </li>
+          </ul>
         </ItemMenu>
         <ItemMenu>Sobre nossa loja</ItemMenu>
         <ItemMenu>Contato</ItemMenu>
