c8336ac2214bfbaa4aaf5ed888345936a41441bf
Leonardo Lüders <luderser@hotmail.com>
2025-06-02 20:26:06 -0300

    Testing google maps access link using the value of location property.

--- src/components/AddressCard/index.tsx
+++ src/components/AddressCard/index.tsx
@@ -8,12 +8,23 @@ type AddressCardProps = {
 export function AddressCard({ address }: AddressCardProps) {
   return (
     <div>
-      <Heading as="h3">Endereço de entrega:</Heading>
+      <Heading as="h3">Local da entrega:</Heading>
       <ul className="list-disc pl-8">
-        <li>{address[0].street}</li>
+        <li>{`${address[0].street}, ${address[0].number}`}</li>
         <li>{address[0].complement}</li>
         <li>{address[0].referencePoint}</li>
         <li>{`${address[0].city} - ${address[0].stateCode}`}</li>
+        <li>
+          <a
+            href={`https://maps.google.com.br/maps?q=
+              ${address[0].location.split(",")[0]}
+              %2C
+              ${address[0].location.split(",")[1]}`}
+            target="_blank"
+          >
+            {`${address[0].location}`}
+          </a>
+        </li>
       </ul>
     </div>
   );
